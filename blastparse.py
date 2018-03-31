524b974a8faa428b404d5cea35199cbb6a9d35b9
David Ray <daray@compute-2-1.localdomain>
2018-03-30 19:52:29 -0500

    final for python class

--- blastparse.py
+++ blastparse.py
@@ -1,3 +1,51 @@
+
 from Bio import SeqIO
-SeqIO.parse("NW-84.Trinity.fasta.transdecoder.pep", "fasta")
+import argparse
+
+def get_args():
+	parser = argparse.ArgumentParser(description="Iterative pseudoreference generation with pseudo-it", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('-i', '--ifasta', type=str, help='Name of the peptide file', required=True)
+	parser.add_argument('-b', '--blastp', type=str, help='Name of the blastp file', required=True)
+	parser.add_argument('-o', '--outprefix', help='Prefix to use on output files', required=True)
+
+	args = parser.parse_args()
+	INFASTA = args.ifasta
+	BLASTP = args.blastp
+	PREFIX = args.outprefix
+	
+	return INFASTA, BLASTP, PREFIX
+#	return INFASTA, PREFIX
+INFASTA, BLASTP, PREFIX = get_args()
+#INFASTA, PREFIX = get_args()
+
+print("Input fasta = " + INFASTA + ".")
+print("Input blastp = " + BLASTP + ".")
+print("The prefix = " + PREFIX + ".")
+
+HITS=[]
+NOHITS=[]
+HITS_FILE = open(PREFIX + '.hits.fas', 'w')
+NOHITS_FILE = open(PREFIX + '.nohits.fas', 'w')
+
+hitcount = 0
+nohitcount = 0
+
+FILECONTENTS = open(BLASTP).read() 
+for RECORD in SeqIO.parse(INFASTA, 'fasta'):
+	if RECORD.id in FILECONTENTS:
+		hitcount = hitcount+1
+		SeqIO.write(RECORD, HITS_FILE, 'fasta')
+	else: 
+		nohitcount = nohitcount+1
+		SeqIO.write(RECORD, NOHITS_FILE, 'fasta')
+
+HITS_FILE.close()
+NOHITS_FILE.close()
+
+print("hitcount = " + str(hitcount))
+print("nohitcount = " + str(nohitcount))
+
+
+	
+
 
